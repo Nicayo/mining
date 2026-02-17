d3cf7d3507b7fc1df31da43e24e3105e1c115676
Nicayo <nicayo714@gmail.com>
2026-02-17 15:10:02 -0300

    register funcional

--- app/controllers/Register.js
+++ app/controllers/Register.js
@@ -1,37 +1,67 @@
 const userName = document.getElementById("userName");
-
 const passWord = document.getElementById("passWord");
 const confirmPassWord = document.getElementById("confirmPassWord");
 
-function validateFields() {
-    switch (true) {
-        case userName.value.length > 20:
-                alert("Nome muito grande");
-            break;
-        case userName.value.length < 5:
-                alert("Nome muito pequeno");
-            break;
+var listUser = [];
 
-        //verificar se esta no sistema
+function registerUser() {
+    if (validateFields() === true) {
+
+        const user = {
+            userName: userName.value,
+            passWord: passWord.value
+        }
+
+        listUser.push(user);
+        localStorage.setItem("user", JSON.stringify(listUser));
+        
+    
+    }
+    
+}
+function validateFields() {
+    
+    if (localStorage.getItem("user") !== null) {
+
+        let duplicated = false;
+
+        JSON.parse(localStorage.getItem("user")).forEach(function(user) {
+                
+            if (user.userName === userName.value) {
+                
+                duplicated = true;
+            }
+                
+        });
+        if (duplicated) alert("usuario ja existente");
+        return !duplicated;
+    }
+        
+
+    switch (true) {
+
+        case userName.value.length > 20:
+            alert("Nome muito grande"); 
+            return false;
+ 
+        case userName.value.length < 5:
+            alert("Nome muito pequeno");  
+            return false;
 
         case passWord.value.length < 8:
-                alert("Sua senha deve ter mais de 8 digitos");
-            break;
-
-        //verificar se esta no sistema
+            alert("Sua senha deve ter mais de 8 digitos");
+            return false;
 
         case confirmPassWord.value != passWord.value:
             alert("Senhas diferentes.");
+            return false;
 
+        default: return true;
 
     }
+
  
 
 }
 
-function registerUser() {
-    validateFields()
-    console.log(userName.value);
-    
-    
-}
+//[{"userName":"nicayo","passWord":"12345678"},{"userName":"nicayo","passWord":"12345678"}]
