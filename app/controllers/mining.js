dafef5932dc8dd4f54aaf208c5276bc15e951ed7
Nicayo <nicayo714@gmail.com>
2026-02-23 07:50:39 -0300

    jogo funcionando, agora falta fazer um sistema de salvamento por login

--- app/controllers/mining.js
+++ app/controllers/mining.js
@@ -17,8 +17,17 @@ let bar = document.getElementById("bar").value
 const buttonBar = document.getElementById("minerar").value
 const nivelNumber = document.getElementById("nivelNumber");
 
+let quantidade = new Map()
+    quantidade.set("pedra", 0);
+    quantidade.set("prata", 0);
+    quantidade.set("ouro", 0);
+    quantidade.set("platina", 0);
+    quantidade.set("Diamante", 0);
+    quantidade.set("Bitcoin", 0);
+
+
 function minerar() {
-    document.getElementById("bar").value +=5
+    document.getElementById("bar").value +=50
     document.getElementById("nivelBar").value +=1
 
     if (document.getElementById("nivelBar").value == 100) {
@@ -67,23 +76,90 @@ function minerar() {
             
     } 
     
+
     
-     let aleatorio = Math.floor((Math.random()* 168) + 1)       
+    let aleatorio = Math.floor((Math.random()* 168) + 1)       
             
     
     let minerioSorteado = array[aleatorio]
     document.getElementById("ultimoMinerioNome").innerHTML = minerioSorteado
     
-}}
-
-
     
 
+    switch(minerioSorteado) {
+        case "pedra":
+            quantidade.get("pedra")
+            quantidade.set("pedra", quantidade.get("pedra") + 1)
+            let minerio = quantidade.get("pedra")
+            let pedra = document.getElementById("minerioPedra");
+            let texto = pedra.innerText;
+            if (texto.length >= 8) {
+                let novoTexto = texto.substring(0, 8) + minerio
+                pedra.innerText = novoTexto;
+            }
+        break;
 
+        case "prata":
+            quantidade.get("prata")
+            quantidade.set("prata", quantidade.get("prata") + 1)
+            let minerio1 = quantidade.get("prata")
+            let prata = document.getElementById("minerioPrata");
+            let texto1 = prata.innerText;
+            if (texto1.length >= 7) {
+                let novoTexto = texto1.substring(0, 7) + minerio1
+                prata.innerText = novoTexto;
+            }
+        break;
 
+        case "ouro":
+            quantidade.get("ouro")
+            quantidade.set("ouro", quantidade.get("ouro") + 1)
+            let minerio2 = quantidade.get("ouro")
+            let Ouro = document.getElementById("minerioOuro");
+            let texto2 = Ouro.innerText;
+            if (texto2.length >= 6) {
+                let novoTexto = texto2.substring(0, 6) + minerio2
+                Ouro.innerText = novoTexto;
+            }
+        break
 
+        case "platina":
+            quantidade.get("platina")
+            quantidade.set("platina", quantidade.get("platina") + 1)
+            let minerio3 = quantidade.get("platina")
+            let platina = document.getElementById("minerioPlatina");
+            let texto3 = platina.innerText;
+            if (texto3.length >= 9) {
+                let novoTexto = texto3.substring(0, 9) + minerio3
+                platina.innerText = novoTexto;
+            }
+        break
 
+        case "Diamante":
+            quantidade.get("Diamante")
+            quantidade.set("Diamante", quantidade.get("Diamante") + 1)
+            let minerio4 = quantidade.get("Diamante")
+            let diamante = document.getElementById("minerioDiamante");
+            let texto4 = diamante.innerText;
+            if (texto4.length >= 10) {
+                let novoTexto = texto4.substring(0, 10) + minerio4
+                diamante.innerText = novoTexto;
+            }
+        break
 
+        case "BitCoin":
+            quantidade.get("Bitcoin")
+            quantidade.set("Bitcoin", quantidade.get("Bitcoin") + 1)
+            let minerio5 = quantidade.get("Bitcoin")
+            let bitcoin = document.getElementById("minerioBitCoin");
+            let texto5 = bitcoin.innerText;
+            if (texto5.length >= 9) {
+                let novoTexto = texto5.substring(0, 9) + minerio5
+                bitcoin.innerText = novoTexto;
+            }
+        break
+    }
+    console.log(quantidade)
+    
+}}
 
-
-         
